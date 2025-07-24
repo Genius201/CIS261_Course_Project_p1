e16cd380d5f8071caa18207ebc2ce8ed94a2e6e2
username <useremail>
2025-07-23 22:46:15 -0700

    correct code

--- CIS261_Course_Project_p1.py
+++ CIS261_Course_Project_p1.py
@@ -1,21 +1,21 @@
 def get_employee_name():
     while True:
-        employee_name = input('Enter employee name:').strip()
-        if employee_name:
+        employee_name = input('Enter employee name: ')
+        if employee_name.strip():
             return employee_name
         else:
-            print('Employee name cannot be empty. Please enter a name.')
+            print('Employee name cannot be empty. Please enter a name. ')
 
 def get_total_hours():
     while True:
         try:
-            hours = float(input('Enter total hours worked: '))
-            if hours >=0:
-                return hours
-            else:
-                print('Hours cannot be negative. Please enter a valid number.')
+           hours = float(input('Enter total hours worked: '))
+           if hours >=0:
+               return hours
+           else:
+               print('Hours cannot be negative. Please enter a valid number.')
         except ValueError:
-                print('Invalid input. Please enter a numerical value.')
+            print('Invalid input. Please enter a numerical value.')
 
 def get_hourly_rate():
     while True:
@@ -31,17 +31,19 @@ def get_hourly_rate():
 def get_income_tax_rate():
     while True:
         try:
-            tax_rate = float(input('Enter income tax rate(as a decimal): '))
-            if 0 <= tax_rate <=1:
-                return tax_rate
-            else:
-                print('Income tax rate must be between 0 and 1.')
+           tax_rate = float(input('Enter income tax rate(as a decimal): '))
+           if 0 <= tax_rate <=1:
+               return tax_rate
+           else:
+               print('Income tax rate must be between 0 and 1.')
         except ValueError:
-            print('Invalid Input.')
+           print('Invalid Input.')
 
 def calculate_pay(total_hours, hourly_rate, tax_rate):
+    
+    
     gross_pay = total_hours * hourly_rate
-    income_tax = gross_pay - income_tax
+    income_tax = gross_pay * tax_rate
     net_pay = gross_pay - income_tax
     return gross_pay, net_pay, income_tax
 
@@ -74,29 +76,31 @@ def main():
 
     print('Welcome To The Payroll Calculator!')
     print('Type "end" for Employee name to terminate the program.')
+   
     while True:
         employee_name = get_employee_name()
         if employee_name.lower() == 'end':
-                                          break
+            break
                                        
-                                           total_hours = get_total_hours()
-    hourly_rate = get_hourly_rate()
-    income_tax_rate = get_income_tax_rate()
+        total_hours = get_total_hours()
+        hourly_rate = get_hourly_rate()
+        income_tax_rate = get_income_tax_rate()
 
-      gross_pay, net_pay, income_tax_amount = calculate_pay(total_hours, hourly_rate, income_tax_rate)
+        gross_pay, net_pay, income_tax_amount = calculate_pay(total_hours, hourly_rate, income_tax_rate)
 
-display_employee_details(employee_name, total_hours, hourly_rate, gross_pay, income_tax_rate, income_tax_amount, net_pay)
+        display_employee_details(employee_name, total_hours, hourly_rate, gross_pay, income_tax_rate, income_tax_amount, net_pay)
 
-total_employees += 1
-total_hours_sum += total_hours
-total_gross_pay_sum += gross_pay
-total_tax_sum += income_tax_amount
-total_net_pay_sum += net_pay
+        total_employees += 1
+        total_hours_sum += total_hours
+        total_gross_pay_sum += gross_pay
+        total_tax_sum += income_tax_amount
+        total_net_pay_sum += net_pay
 
-if total_employees > 0:
-    display_totals(total_employees, total_hours_sum, total_gross_pay_sum, total_tax_sum, total_net_pay_sum)
-else:
-    print('\nNo employee data was entered')
+    if total_employees > 0:
+        display_totals(total_employees, total_hours_sum, total_gross_pay_sum, total_tax_sum, total_net_pay_sum)
+    else:
+        print('\nNo employee data was entered')
+        
     print('\nThank you for using payroll calculator!')
 
 if __name__=='__main__':
